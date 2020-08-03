d10de2288a313f13c859d87f37dfcb43e5619c8a
mark <mark@mark-desktop>
2020-08-03 15:17:10 -0400

    try this again

---
+++ src/mtrmstnmr.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : mtrmstnmr.cpp
+// Author      : Mark Meadows
+// Version     :
+// Copyright   : copyright 2018
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "testMEI" << endl; // prints testMEI
+	return 0;
+}
